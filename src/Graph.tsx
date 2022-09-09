37b80c5c57dacbc969759ddeefab5949c7455f13
joao badaro <joaobadaro@gmail.com>
2022-09-09 18:53:07 -0300

    criação de relacionamento entre nos

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,12 +1,19 @@
 import { useEffect, useId, useRef, useState } from "react";
 import { layouts } from "./layouts";
 import CytoscapeComponent from "react-cytoscapejs";
-import useLocalStorage from "./hooks/useLocalStorage";
 
 interface INode {
   id: string;
   label: string;
+  difficulty: number;
+  isVisited: boolean;
 }
+interface IEdge {
+  target: string;
+  source: string;
+  label: number;
+}
+
 interface IClickedPosition {
   x: number;
   y: number;
@@ -15,37 +22,42 @@ export function Graph() {
   const cyRef = useRef<cytoscape.Core | undefined>();
   const [layout, setLayout] = useState(layouts.klay);
   const [isCreatingNode, setIsCreatingNode] = useState(false);
-  const [isFirstNodeSelected, setIsFirstNodeSelected] = useState(false);
+  const [isNodeSelected, setIsNodeSelected] = useState(false);
   const [primaryNode, setPrimaryNode] = useState<INode | null>(null);
-  const [numberOfNodes, setNumberOfNodes] = useState<number>(6);
+  const [relationship, setRelationship] = useState<INode[] | []>([]);
+  const [numberOfNodes, setNumberOfNodes] = useState<number>(7);
   const [secondaryNode, setSecondaryNode] = useState<INode | null>(null);
   const [clickedPosition, setClickedPosition] = useState<IClickedPosition>();
+  const [selectedEdge, setSelectedEdge] = useState<IEdge | null>(null);
   const defaultGraph = [
     {
-      data: { id: "1", label: "Node 1" },
+      data: { id: "1", label: "Node 1", difficulty: 5, isVisited: false },
       position: { x: 600, y: 150 },
     },
 
     {
-      data: { id: "66", label: "Node 88" },
+      data: { id: "66", label: "Node 88", difficulty: 5, isVisited: false },
       position: { x: 1000, y: 150 },
     },
 
-    { data: { id: "2", label: "Node 2" }, position: { x: 500, y: 300 } },
     {
-      data: { id: "3", label: "Node 3" },
+      data: { id: "2", label: "Node 2", difficulty: 5, isVisited: false },
+      position: { x: 500, y: 300 },
+    },
+    {
+      data: { id: "3", label: "Node 3", difficulty: 5, isVisited: false },
       position: { x: 100, y: 200 },
     },
     {
-      data: { id: "4", label: "Node 4" },
+      data: { id: "4", label: "Node 4", difficulty: 5, isVisited: false },
       position: { x: 300, y: 100 },
     },
     {
-      data: { id: "5", label: "Node 5" },
+      data: { id: "5", label: "Node 5", difficulty: 5, isVisited: false },
       position: { x: 400, y: 50 },
     },
     {
-      data: { id: "6", label: "Node 6" },
+      data: { id: "6", label: "Node 6", difficulty: 5, isVisited: false },
       position: { x: 400, y: 200 },
     },
     {
@@ -165,36 +177,54 @@ export function Graph() {
   useEffect(() => {
     if (clickedPosition && isCreatingNode) {
       const newId = numberOfNodes + 1;
-      setElements((oldState) => [
-        ...oldState,
-        {
-          data: { id: newId.toString(), label: "Teste" },
-          position: { x: clickedPosition.x, y: clickedPosition.y },
-        },
-      ]);
-
-      window.localStorage.setItem("elements", JSON.stringify(elements));
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [clickedPosition]);
-
-  useEffect(() => {
-    if (secondaryNode) {
+      console.log("number of nides", numberOfNodes);
       setElements((oldState) => [
         ...oldState,
         {
           data: {
-            source: primaryNode ? primaryNode?.id : "",
-            target: secondaryNode ? secondaryNode?.id : "",
-            label: "Edge from Node1 to Node2",
+            id: newId.toString(),
+            label: "Teste",
+            difficulty: 5,
+            isVisited: false,
+          },
+          position: { x: clickedPosition.x, y: clickedPosition.y },
+        },
+      ]);
+
+      setNumberOfNodes(newId + 1);
+      window.localStorage.setItem("elements", JSON.stringify(elements));
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [clickedPosition]);
+  useEffect(() => {
+    if (relationship.length === 2) {
+      console.log("entrei if 2");
+      setElements((oldState) => [
+        ...oldState,
+        {
+          data: {
+            source: relationship[0] ? relationship[0]?.id : "",
+            target: relationship[1] ? relationship[1]?.id : "",
+            label: `Edge from ${relationship[0]?.id} to ${relationship[1]?.id}`,
           },
         },
       ]);
+      setRelationship([]);
     }
-
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [secondaryNode]);
+  }, [relationship]);
 
+  const createRelationship = (node: INode) => {
+    console.log("entrei", node);
+    if (relationship.length < 2) {
+      setRelationship([...relationship, node]);
+      console.log(
+        "🚀 ~ file: Graph.tsx ~ line 204 ~ createRelationship ~ setRelationship",
+        relationship
+      );
+      console.log("entrei if 1", relationship);
+    }
+  };
   const containerStyle = {
     width: "1200px",
     height: "700px",
@@ -202,20 +232,8 @@ export function Graph() {
   };
 
   const deleteNode = (selectedNode: any) => {
-    const newElements = elements.filter((node) => {
-      if (node.data.id !== selectedNode.id) {
-        console.log("node.data.id", node.data.id);
-        return true;
-      } else {
-        return false;
-      }
-    });
-    console.log(
-      "🚀 ~ file: Graph.tsx ~ line 220 ~ newElements ~ newElements",
-      newElements
-    );
-
-    setElements(newElements);
+    if (selectedNode === undefined) return;
+    cyRef.current?.remove(`#${selectedNode?.id}`);
   };
   return (
     <div className="wrapper">
@@ -245,12 +263,12 @@ export function Graph() {
                 "text-max-width": "200px",
                 label: function (node: any) {
                   const nodeAtribute = node.data();
-                  let labelFinal = "";
-                  Object.keys(nodeAtribute).map(function (key, index) {
-                    return (labelFinal += ` ${key} = ${nodeAtribute[key]} `);
-                  });
-
-                  return labelFinal;
+                  // let labelFinal = "";
+                  // Object.keys(nodeAtribute).map(function (key, index) {
+                  //   return (labelFinal += ` ${key} = ${nodeAtribute[key]} `);
+                  // });
+                  // return labelFinal;
+                  return `id = ${nodeAtribute.id}`;
                 },
               },
             },
@@ -263,46 +281,87 @@ export function Graph() {
             },
           ]}
           cy={(cy) => {
-            cy.on("tap", function (event) {
-              setNumberOfNodes(cy.nodes().length);
+            cyRef.current = cy;
+            setNumberOfNodes(cy.nodes().length + 1);
+            cy.on("tap", "node", function (event) {
               var evtTarget = event.target;
-              // cy.$("#j").neighborhood((ele: any) => {
-              //   return ele.isNode();
-              // });
+              var node = event.target;
+              let clickedElement = node._private.data;
+              if (node._private.nodeKey === null) {
+                setSelectedEdge(clickedElement);
+                setPrimaryNode(null);
+                setIsNodeSelected(false);
+              } else if (node._private.nodeKey !== null) {
+                setPrimaryNode(clickedElement);
+                createRelationship(clickedElement);
+                setSelectedEdge(null);
+                setIsNodeSelected(true);
+              }
+            });
+            cy.on("tap", function (event) {
+              var evtTarget = event.target;
 
-              // console.log(je.connectedNodes());
-
+              //clicked on canvas
               if (evtTarget === cy) {
                 if (isCreatingNode) setClickedPosition(event.position);
+                setPrimaryNode(null);
+                //clicked on node or edge
               } else {
                 var node = event.target;
-                let clickedNodedata = node._private.data;
-
-                if (node._private.nodeKey !== null && !isFirstNodeSelected) {
-                  setPrimaryNode(clickedNodedata);
-                  console.log("entrei IF1", !isFirstNodeSelected);
-                  setIsFirstNodeSelected(true);
-                } else if (
-                  node._private.nodeKey !== null &&
-                  !isFirstNodeSelected
-                ) {
-                  setSecondaryNode(clickedNodedata);
+                let clickedElement = node._private.data;
+                if (node._private.nodeKey === null) {
+                  setSelectedEdge(clickedElement);
+                  setPrimaryNode(null);
+                  setIsNodeSelected(false);
                 }
               }
             });
           }}
         />
         <div>
-          <h1>Data</h1>
-          <button onClick={() => deleteNode(primaryNode)}>Delete Node</button>
-          <div className="subtitle">
-            <h2>Node ID</h2>
-            <h3>{primaryNode?.id}</h3>
-          </div>
-          <div className="subtitle">
-            <h2>Node Label</h2>
-            <h3>{primaryNode?.label}</h3>
-          </div>
+          <>
+            <div className="header">
+              <h1>Data</h1>
+              <button onClick={() => deleteNode(primaryNode)}>
+                Delete Node
+              </button>
+            </div>
+            <div className="subtitle">
+              <h2>Node ID</h2>
+              <h3>{primaryNode?.id}</h3>
+            </div>
+            <div className="subtitle">
+              <h2>Node Label</h2>
+              <h3>{primaryNode?.label}</h3>
+            </div>
+            <div className="subtitle">
+              <h2>Difficulty</h2>
+              <h3>{primaryNode?.difficulty}</h3>
+            </div>
+            <div className="subtitle">
+              <h2>Visited</h2>
+              <h3>{primaryNode?.isVisited ? "true" : "false"}</h3>
+            </div>
+          </>
+
+          <>
+            <div className="header">
+              <h1>Edge Data</h1>
+            </div>
+            <div className="subtitle">
+              <h2>Source</h2>
+              <h3>{selectedEdge?.source}</h3>
+            </div>
+            <div className="subtitle">
+              <h2>Target</h2>
+              <h3>{selectedEdge?.target}</h3>
+            </div>
+
+            <div className="subtitle">
+              <h2>Label</h2>
+              <h3>{selectedEdge?.label}</h3>
+            </div>
+          </>
         </div>
       </div>
     </div>
